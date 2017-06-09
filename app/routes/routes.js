623c83a2b7e612f2b6708a42953eeed17fcdb206
XxShadowWolfxX <alim.0158@gmail.com>
2017-06-08 20:55:29 -0400

    6-8-17

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,10 +1,24 @@
 var r = require("./routes.json");
 
 var controllers = {
-	pages : require("./controllers/pages")
-}
+	pages : require("./controllers/pages"),
+	session : require("./controllers/session"),
+	blogCtrl : require("./controllers/blogCtrl")
+};
 
-module.exports = function(app) {
-	app.get(r.index, controllers.pages.index)
-	app.get(r.create, controllers.pages.create)
+module.exports = function(app, passport) {
+	app.get(r.home, controllers.pages.home);
+	app.get(r.login, controllers.pages.login);
+	app.get(r.index, controllers.pages.index);
+	app.get(r.post, controllers.pages.post);
+	app.get(r.create, controllers.blogCtrl.createPage);
+	app.get(r.update, controllers.blogCtrl.updatePage);
+	app.post(r.signup, controllers.pages.signup);
+	app.post(r.login, passport.authenticate("local-login", {
+		successRedirect : r.index,
+		failureRedirect : r.login
+	}));
+	app.post(r.create, controllers.session, controllers.blogCtrl.create);
+	app.put(r.create, controllers.session, controllers.blogCtrl.update);
+	app.delete(r.delete, controllers.session, controllers.blogCtrl.delete);
 }
--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var express = require("express"),
     session = require("express-session"),
     mongoose = require("mongoose"),
     passport = require("passport"),
+    methodOverride = require("method-override"),
+    auth = require("./app/auth/passport-local"),
     routes = require("./app/routes/routes"),
     app = express();
 
@@ -22,12 +24,15 @@ app.use(session({
     saveUninitialized: true
 }));
 
+app.use(methodOverride('methtod'));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.set("views", path.join(__dirname, "app/views"));
 app.set("view engine", "hbs");
 
+auth(passport);
 routes(app, passport);
 
 mongoose.connect("mongodb://localhost/blog");
